291d975dbba29398b0c27bdbe880206693dcc2d0
01KingMaker <fabrakoto0987@gmail.com>
2024-04-27 20:04:47 +0300

    add personal chat bot

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from '../services/app.service';
 import { UploaderModule } from 'src/uploader/uploader.module';
 import { ReplicateModule } from 'src/replicate/replicate.module';
 import { HandlerGateWay } from 'src/socket/handler.gateway';
+import { BrainShopModule } from 'src/brain-shop/brain-shop.module';
 
 @Module({
   imports: [
-    UploaderModule, ReplicateModule
+    UploaderModule, ReplicateModule, BrainShopModule
   ],
   controllers: [AppController],
   providers: [AppService, HandlerGateWay],
